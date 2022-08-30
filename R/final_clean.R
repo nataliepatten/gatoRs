ebfb240f1dc123c4a74925f7bcc67e2ddbe4ab89
Shelly Gaynor <michellegaynor@knights.ucf.edu>
2022-08-30 10:09:01 -0400

    Added line with dismo function. Needs to be tested and added properly!

--- R/final_clean.R
+++ R/final_clean.R
@@ -5,34 +5,34 @@
 #' duplicate data points, checking locality precision, and
 #' retaining only one collection point.
 #' @details
-#' This function requires packages dplyr, base, raster, spatstat
+#' This function requires packages dplyr, base, raster, dismo
 #'
-#' @param df is a dataframe of occurrence records
-#' @param cluster is an option (TRUE/FALSE) to cluster geographically close occurrence records. This option is turned on by default.
-#' @param precision in an option to round coordinates to the specified number of digits. This option is set to 2 by default. To
+#' @param df Data frame of occurrence records.
+#' @param cluster An option (TRUE/FALSE) to cluster geographically close occurrence records. This option is turned on by default.
+#' @param precision An option to round coordinates to the specified number of digits. This option is set to 2 by default. To
 #' not use any rounding, choose `precision = NA`.
-#' @param recordBasis is an option (TRUE/FALSE) to interactively remove types of basis of record. This option is turned on by default.
+#' @param recordBasis An option (TRUE/FALSE) to interactively remove types of basis of record. This option is turned on by default.
 #'
-#' @return df is a dataframe with the cleaned data
+#' @return df is a data frame with the cleaned data.
+#'
+#'
+#' @importFrom raster raster
+#' @importFrom dismo gridSample
 #'
 #' @export
 
 final_clean <- function(df, cluster = TRUE, precision = 2, recordBasis = TRUE) {
-  if (cluster) {
-    # one point per pixel
-    # bio1 <- raster::raster("data/CLIMATE/bio1.bil")
-    # rasterResolution <- max(res(bio1))
-    # while(min(spatstat::nndist(df[,2:3])) < rasterResolution){
-    #   nnD <- spatstat::nndist(df[,2:3])
-    #   df <- df[-(which(min(nnD) == nnD)[1]),]
-    # }
-  }
 
   # round for precision
   if (!is.na(precision)) {
     df$latitude <- round(df$latitude, digits = precision)
     df$longitude <- round(df$longitude, digits = precision)
+
+   # bio1 <- raster::raster("data/CLIMATE/bio1.bil")
+
+    # out <- dismo::gridSample(df, bio1, n = 1) # add to function
   }
+
 
   # remove duplicates
   df <- dplyr::distinct(df, longitude, latitude, .keep_all = TRUE)
