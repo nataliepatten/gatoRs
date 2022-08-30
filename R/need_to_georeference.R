d59fcdc2bb80d884ebfa8bc71b4548c1083eb4cf
Shelly Gaynor <michellegaynor@knights.ucf.edu>
2022-08-30 10:25:20 -0400

    description updated

--- R/need_to_georeference.R
+++ R/need_to_georeference.R
@@ -4,12 +4,12 @@
 #' This function allows you to find records that are missing coordinates but have locality information.
 #'
 #' @details
-#' This function requires packages dplyr, base, magrittr.
+#' This function requires packages dplyr.
 #' This function does not require user input.
 #'
 #' @param occurrence_records A data frame of occurrence records.
 #'
-#' @return for_georeferencing is a data frame of the points that need to be georeferenced.
+#' @return for_georeferencing is a data frame of the points that need to be georeferenced. For more information about this data frame, see `gators_download()`.
 #'
 #' @importFrom dplyr filter
 #'
@@ -17,12 +17,14 @@
 
 
 need_to_georeference <- function(occurrence_records){
+
   for_georeferencing <- occurrence_records %>%
-    dplyr::filter(locality != 'NA') %>%
-    dplyr::filter(is.na(latitude)) %>%
-    dplyr::filter(is.na(longitude))
+                        dplyr::filter(locality != 'NA') %>%
+                        dplyr::filter(is.na(latitude)) %>%
+                        dplyr::filter(is.na(longitude))
   # uuid_list <- for_georeferencing$uuid
   # idigbio_info <- dplyr::bind_rows(lapply(uuid_list, function(x) idig_search_records(rq = list(uuid = x), fields=c("uuid", "data.dwc:catalogNumber", "scientificname", "collectionname", "data.dwc:datasetName",  "data.dwc:institutionCode"))))
   # for_georeferencing_all <- dplyr::left_join(for_georeferencing, idigbio_info, by = "uuid")
+
   return(for_georeferencing)
 }
