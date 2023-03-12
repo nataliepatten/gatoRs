700c6e81e49ef53dbc99a1d266acc778304b6dbb
Natalie Patten <natalienpatten@gmail.com>
2023-03-11 22:07:46 -0500

    add data obj for running examples

---
+++ R/data.R
@@ -0,0 +1,5 @@
+#' Downloaded data from gators_download() for Asclepias curtissii with default settings:
+#' gators_download(c("Asclepias curtissii", "Asclepias aceratoides", "Asclepias arenicola", "Oxypteryx arenicola", "Oxypteryx curtissii"), "newFile.csv")
+#' @format A data frame with 559 rows and 19 columns
+#' @keywords internal
+"data"
