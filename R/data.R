583a39fd14464aa53ef2871765a3dd3637cb3e50
Natalie Patten <natalienpatten@gmail.com>
2023-03-16 23:31:21 -0400

    update data example to be Galax urceolata download

--- R/data.R
+++ R/data.R
@@ -1,5 +1,5 @@
-#' Downloaded data from gators_download() for Asclepias curtissii with default settings:
-#' gators_download(c("Asclepias curtissii", "Asclepias aceratoides", "Asclepias arenicola", "Oxypteryx arenicola", "Oxypteryx curtissii"), "newFile.csv")
-#' @format A data frame with 559 rows and 19 columns
+#' Downloaded data from gators_download() for Galax urceolata with default settings:
+#' data <- gators_download(synonyms_list = c("Galax urceolata", "Galax aphylla"))
+#' @format A data frame with 6885 rows and 19 columns
 #' @keywords internal
 "data"
