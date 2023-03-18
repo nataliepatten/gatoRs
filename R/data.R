2108a4169e231c08b7f2f6bea9b217ce26f3ad40
Natalie Patten <natalienpatten@gmail.com>
2023-03-17 23:17:58 -0400

    modify to reflect new function names, functionality, and examples

--- R/data.R
+++ R/data.R
@@ -1,5 +1,5 @@
 #' Downloaded data from gators_download() for Galax urceolata with default settings:
-#' data <- gators_download(synonyms_list = c("Galax urceolata", "Galax aphylla"))
+#' data <- gators_download(synonyms.list = c("Galax urceolata", "Galax aphylla"))
 #' @format A data frame with 6885 rows and 19 columns
 #' @keywords internal
 "data"
