b3e573dc45922aec2c599e06fa93bcc8d56f6245
Natalie Patten <natalienpatten@gmail.com>
2023-03-17 23:49:50 -0400

    update documentation and args, add checks, remove duplicates first for efficiency

--- R/full_clean.R
+++ R/full_clean.R
@@ -1,22 +1,23 @@
 #' @title Full Clean - Wrapper function to speed clean
 #'
 #' @description
-#' The `final_clean()` function performs final cleaning steps, including: removing
-#' duplicate data points, checking locality precision, and retaining only one collection point.
+#' The `full_clean()` function performs automated cleaning steps, including options for: removing
+#' duplicate data points, checking locality precision, removing points with skewed coordinates,
+#' removing plain zero records, removing records based on basis of record, and clustering collection points.
 #' This function also provides the option to interactively inspect and remove types of basis of record.
 #'
 #' @details
 #' This function requires packages dplyr, magrittr, raster, and dismo.
 #'
 #' @param df Data frame of occurrence records.
-#' @param cluster An option (TRUE/FALSE) to cluster geographically close occurrence records.
-#' This option is turned on by default.
+#' @param cluster Default = TRUE. An option to cluster geographically close occurrence records.
 #' @inheritParams taxa_clean
 #' @inheritParams basic_locality_clean
 #' @param basis.list A list of basis to keep. If a list is not supplied, this filter will not occur.
 #'
 #' @examples
-#' data <- final_clean(data, recordBasis = FALSE)
+#' data <- full_clean(data, synonyms.list = c("Galax urceolata", "Galax aphylla"), digits = 3, accepted.name = "Galax urceolata")
+#' data <- full_clean(data, synonyms.list = "Galax urceolata", remove.skewed = FALSE, basis.list = "HUMAN_OBSERVATION", cluster = FALSE)
 #'
 #' @return df is a data frame with the cleaned data.
 #'
@@ -24,17 +25,26 @@
 #'
 #' @export
 
-final_clean <- function(df,
-                        synonyms_list, taxa.filter = "fuzzy", accepted_name,
-                        remove.zero = TRUE, precision = 2, remove.skewed = TRUE,
-                        basis.list,
-                        cluster = TRUE, ) {
-  df <- suppressMessages(taxa_clean(df = df,  synonyms_list = synonyms_list, taxa.filter = taxa.filter, accepted_name =  accepted_name ))
-  df <- basic_locality_clean(df, remove.zero = remove.zero, precision = precision,
-                             remove.skewed = remove.skewed)
+full_clean <- function(df, synonyms.list, taxa.filter = "fuzzy",
+                        accepted.name = NA, remove.zero = TRUE,
+                        precision = TRUE, digits = 2, remove.skewed = TRUE,
+                        basis.list = NA, cluster = TRUE) {
+
   df <- remove_duplicates(df)
-  if(length(basis.list) > 1){
-  df <- basis_clean(df, basis.list = basis.list)
+  df <- suppressMessages(taxa_clean(df = df,  synonyms.list = synonyms.list, taxa.filter = taxa.filter, accepted.name =  accepted.name ))
+  df <- basic_locality_clean(df, remove.zero = remove.zero, precision = precision,
+                             digits = digits, remove.skewed = remove.skewed)
+  if(!is.na(basis.list)){
+    df <- basis_clean(df, basis.list = basis.list)
   }
 
+  if (cluster != TRUE & cluster != FALSE) {
+    # warning, since the rest of cleaning can occur even if this arg is invalid
+    warning("Invalid value for argument: cluster. Value for cluster must equal 'TRUE' or 'FALSE'.")
+  }
+  else if (cluster == TRUE) {
+    # TO-DO: add cluster option
+  }
+
+  return(df)
 }
