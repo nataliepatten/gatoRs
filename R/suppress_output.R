6b8b53221622273aca1c715239851d6d34bd50df
Natalie Patten <natalienpatten@gmail.com>
2023-03-19 16:38:25 -0400

    add function to suppress prints and messages

---
+++ R/suppress_output.R
@@ -0,0 +1,17 @@
+#' @title Suppress print statements and messages
+#'
+#' @description
+#' The `suppress_output()` function suppresses print statements and messages from a function. This
+#' does not suppress warnings or errors.
+#'
+#'
+#' @param function
+#'
+#'
+#' @return output Output is the output of the passed function call.
+#'
+#' @keywords internal
+
+suppress_output <- function(input) {
+  return(suppressMessages(invisible(capture.output(input))))
+}
