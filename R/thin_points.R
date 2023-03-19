bd721d0ec00b14d40024812813cc39b0f3fdf5aa
Natalie Patten <natalienpatten@gmail.com>
2023-03-19 16:41:30 -0400

    add calls to suppress_ouput() and fix bug with data frame returned from spThin with multiple reps

--- R/thin_points.R
+++ R/thin_points.R
@@ -23,18 +23,19 @@
 
 thin_points <- function(df, accepted.name = NA, distance = 5, reps = 100) {
   if (is.na(accepted.name) & !("accepted_name" %in% colnames(df))) {
-    stop("Error: The data frame does not already have an accepted_name column. \nPlease provide a value for argument accepted.name.")
+    stop("The data frame does not already have an accepted_name column. \nPlease provide a value for argument accepted.name.")
   }
   else if (is.na(accepted.name) & ("accepted_name" %in% colnames(df))) {}
   else {
     df$accepted_name <- accepted.name
   }
 
-  thin_data <- spThin::thin(df, lat.col="latitude", long.col = "longitude",
+  suppress_output(
+    thin_data <- spThin::thin(df, lat.col="latitude", long.col = "longitude",
                      spec.col = "accepted_name", thin.par = distance,
                      reps = reps, write.files = FALSE, write.log.file = FALSE,
-                     locs.thinned.list.return = TRUE)
-  thin_data <- thin_data[[1]]
+                     locs.thinned.list.return = TRUE))
+  thin_data <- thin_data[[reps]]
   df <- df[df$latitude %in% thin_data$Latitude & df$longitude %in% thin_data$Longitude, ]
 
   return(df)
