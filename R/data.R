95fa2d0ba537e56e5f8f2ce219081f5ebaa88419
Natalie Patten <natalienpatten@gmail.com>
2023-04-01 17:43:14 -0400

    update example data to be fewer rows

--- R/data.R
+++ R/data.R
@@ -1,5 +1,5 @@
-#' Downloaded data from gators_download() for Galax urceolata with default settings:
-#' data <- gators_download(synonyms.list = c("Galax urceolata", "Galax aphylla"))
-#' @format A data frame with 6885 rows and 19 columns
+#' Downloaded data from gators_download() for Galax urceolata with default settings and 'limit' set to 1000:
+#' data <- gators_download(synonyms.list = c("Galax urceolata", "Galax aphylla"), limit = 1000)
+#' @format A data frame with 3216 rows and 20 columns
 #' @keywords internal
 "data"
