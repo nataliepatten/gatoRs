56d4bcb8ddcbd0088b3a9ff4ca1053ab9d2e874d
Natalie Patten <natalienpatten@gmail.com>
2023-04-15 22:10:06 -0400

    continue work on remove_duplicates

--- R/remove_duplicates.R
+++ R/remove_duplicates.R
@@ -1,7 +1,7 @@
 #' @title Remove Duplicates - Remove records with identical event dates and coordinates
 #'
 #' @description
-#' The `remove_duplicates()` function removes records with identical event dates and coordinate values.
+#' The `remove_duplicates()` function removes records with identical event dates and occurrence IDs.
 #' Prior to utilizing this function, longitude and latitude columns should be rounded to match the
 #' coordinate uncertainty using the `basic_locality_clean()` function.
 #'
@@ -10,72 +10,59 @@
 #'
 #' @param df Data frame of occurrence records returned from `gators_download()`.
 #' @inheritParams correct_class
+#' @param remove.NA.occ.id Default = FALSE. This will remove records with missing occurrence IDs when set to `TRUE`.
+#' @param remove.NA.date Default = FALSE. This will remove records with missing event dates when set to `TRUE`.
 #'
 #' @examples
 #' data <- remove_duplicates(data)
 #'
 #' @return Return data frame with duplicates removed.
 #'
-#' @importFrom lubridate ymd year month day
 #' @importFrom dplyr distinct
 #'
 #' @export
-#'
-#'
 
 remove_duplicates <- function(df, event.date = "eventDate",
                               latitude = "latitude", longitude = "longitude",
-                              aggregator = "aggregator", id = "ID", occ.id = "occurrenceID"){
+                              aggregator = "aggregator", id = "ID", occ.id = "occurrenceID",
+                              year = "year", month = "month", day = "day",
+                              remove.NA.occ.id = FALSE, remove.NA.date = FALSE){
+
   if (NROW(df) == 0) return(df)
+
+  if (remove.NA.occ.id == TRUE) {
+    df <- df[!is.na(df[[occ.id]]), ]
+  }
+  if (remove.NA.date == TRUE) {
+    df <- df[!is.na(df[[event.date]]), ]
+  }
 
   # Remove within aggregation duplicates based on ID (UUID or KEY)
   ## This is done within our download function as well
   ag <- unique(df[[aggregator]])
   tempdf <- c()
   for(i in 1:length(ag)){
-    tempdf[[i]] <-  df[df[aggregator] == ag[i], ]
-    if(((length(unique(na.omit(tempdf[[i]][[id]])))) == (nrow(na.omit(tempdf[[i]][id])))) == FALSE){
+    tempdf[[i]] <- df[df[aggregator] == ag[i], ]
+    if( (length(unique(na.omit(tempdf[[i]][[id]])))) != nrow(na.omit(tempdf[[i]][id])) ) {
       tempdf[[i]] <- dplyr::distinct(tempdf[[i]], .data[[id]], .keep_all = TRUE)
     }
   }
   df <- do.call(rbind, tempdf)
 
   # Remove specimen duplicates
-  # Format dates
 
-  #for(i in 1:nrow(df)){
-   # if(!is.na(df[[eventDate]])){
+  # Populate event.date column based on year, month, day columns
+  df[[event.date]] <- dplyr::case_when(is.na(df[[event.date]]) ~ paste(df[[year]], df[[month]], df[[day]], sep="-"),
+                                      .default = as.character(df[[event.date]]))
 
-  #  dashcount <- stringr::str_count(random, "[-]")
-  #  if(dashcount == 0){
-    #  df$year[i] <- lubridate::year(as.Date(tempdate, "%Y"))
-     # df$month[i] <- NA
-     # df$day[i] <- NA
-   # } else if(dashcount == 1){
-   #   tempdate <- as.Date(tempdate, "%Y-%m")
-   #   df$year[i] <- lubridate::year(tempdate)
-  #    df$month[i] <- lubridate::month(tempdate)
-   #   df$day[i] <- NA
-   # }else if(dashcount == 2){
-  #    tempdate <- as.Date(tempdate, "%Y-%m-%d")
-  #    df$year[i] <- lubridate::year(tempdate)
-   #   df$month[i] <- lubridate::month(tempdate)
-  ##    df$day[i] <- lubridate::day(tempdate)
-  #  }else{
-# #     df$year <- NA
- #     df$month[i] <- NA
- #     df$day[i] <- NA}}
- # }
+  for(i in 1:nrow(df)){
+   if(!is.na(df[[event.date]][i])){
+     # df[[event.date]][i] <- parse event date
+     #parsedate::parse_iso_8601(parsedate::format_iso_8601(df[[event.date]][i]))
+   }
+  }
+  # Remove rows with identical occurrence ID and date
+  df <- dplyr::distinct(df, .data[[occ.id]], .data[[event.date]], .keep_all = TRUE)
 
-  # Parse date with Lubridate
-  suppressWarnings(df[[event.date]] <-  lubridate::ymd(df[[event.date]]))
-  df$year <- lubridate::year(df[[event.date]])
-  df$month <- lubridate::month(df[[event.date]])
-  df$day <- lubridate::day(df[[event.date]])
-  # Remove rows with identical latitude, longitude, year, month, and day
-  df <- dplyr::distinct(df, .data[[latitude]], .data[[longitude]], year, month, day, .keep_all = TRUE)
-  # Removes extra columns
-  df <- df[ , -which(names(df) %in% c("year", "month", "day"))]
   return(df)
-
 }
