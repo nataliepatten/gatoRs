18db5eca7281c44131c7403cebf41d7d07228034
mgaynor1 <shellygaynor@Shellys-MacBook-Pro.local>
2026-08-22 18:32:53 -0400

    minor edit

--- gatoRs.Rcheck/00_pkg_src/gatoRs/R/global_vars.R
+++
@@ -1,1 +0,0 @@
-utils::globalVariables(c("species", ".data", "ID", "temp"))
--- gatoRs.Rcheck/00_pkg_src/gatoRs/inst/doc/Introduction.R
+++
@@ -1,6 +0,0 @@
-## ----echo = FALSE, fig.align='center'-----------------------------------------
-knitr::include_graphics("img/map1.png", dpi = 200)
-
-## ----echo = FALSE, fig.align='center'-----------------------------------------
-knitr::include_graphics("img/map2.png", dpi = 200)
-
